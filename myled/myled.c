6ab0058aee1e6b009a31a5eb7dc73eb2204580dc
Atsushi Yamaguchi <yamaguchi.atsushi2000@gmail.com>
2022-02-09 21:36:06 +0900

    make char device

--- myled/myled.c
+++ myled/myled.c
@@ -1,5 +1,6 @@
 #include<linux/module.h>
 #include<linux/fs.h>
+#include<linux/cdev.h>
 
 MODULE_AUTHOR("Atsushi Yamaguchi");
 MODULE_DESCRIPTION("driver for LED control");
@@ -7,20 +8,38 @@ MODULE_LICENSE("GPL");
 MODULE_VERSION("0.0.1");
 
 static dev_t dev;
+static struct cdev cdv;
+
+static ssize_t led_write(struct file* filp, const char* buf, size_t count, loff_t* pos){//後始末
+    printk(KERN_INFO "led_write is called\n");
+    return 1;
+}
+
+static struct file_operations led_fops = {
+    .owner = THIS_MODULE,
+    .write = led_write
+};
 
 static int __init init_mod(void){//カーネルモジュールの初期化
     int retval;
-    retval = alloc_chrdev_region(&dev ,0 ,1, "myled");
-    if(retval <0){
+    retval = alloc_chrdev_region(&dev ,0 ,1 ,"myled");
+    if(retval < 0){
         printk(KERN_ERR "alloc_chrdev_region failed.\n");
         return retval;
-    }
-
+    }    
     printk(KERN_INFO "%s is loaded. major:%d\n",__FILE__,MAJOR(dev));
+
+    cdev_init(&cdv, &led_fops);
+    retval = cdev_add(&cdv, dev, 1);
+    if(retval <0){
+        printk(KERN_ERR "cdev_add failed. major:%d, minor:%d\n",MAJOR(dev),MINOR(dev));
+        return retval;
+    }
     return 0;
 }
 
 static void __exit cleanup_mod(void){//後始末
+    cdev_del(&cdv);
     unregister_chrdev_region(dev, 1);
     printk(KERN_INFO "%s is unloaded. major:%d\n",__FILE__,MAJOR(dev));
 }
