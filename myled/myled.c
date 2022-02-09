f18f1deff49438b28cce742e01b05cb069256de2
Atsushi Yamaguchi <yamaguchi.atsushi2000@gmail.com>
2022-02-09 18:34:21 +0900

    add log infomation

--- myled/myled.c
+++ myled/myled.c
@@ -1,12 +1,12 @@
 #include<linux/module.h>
 
-
 static int __init init_mod(void){//カーネルモジュールの初期化
+    printk(KERN_INFO "%s is loaded.\n",__FILE__);
     return 0;
 }
 
 static void __exit cleanup_mod(void){//後始末
-
+    printk(KERN_INFO "%s is unloaded.\n",__FILE__);
 }
 
 module_init(init_mod);//マクロ関数を登録
