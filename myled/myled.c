d832adbc5b06821e39841a854108179217ae01d6
Atsushi Yamaguchi <yamaguchi.atsushi2000@gmail.com>
2022-02-12 16:13:11 +0900

    add class

--- myled/myled.c
+++ myled/myled.c
@@ -1,6 +1,7 @@
 #include<linux/module.h>
 #include<linux/fs.h>
 #include<linux/cdev.h>
+#include<linux/device.h>
 
 MODULE_AUTHOR("Atsushi Yamaguchi");
 MODULE_DESCRIPTION("driver for LED control");
@@ -9,6 +10,7 @@ MODULE_VERSION("0.0.1");
 
 static dev_t dev;
 static struct cdev cdv;
+static struct class *cls = NULL;
 
 static ssize_t led_write(struct file* filp, const char* buf, size_t count, loff_t* pos){//後始末
     printk(KERN_INFO "led_write is called\n");
@@ -35,11 +37,18 @@ static int __init init_mod(void){//カーネルモジュールの初期化
         printk(KERN_ERR "cdev_add failed. major:%d, minor:%d\n",MAJOR(dev),MINOR(dev));
         return retval;
     }
+
+    cls = class_create(THIS_MODULE,"myled");
+    if(IS_ERR(cls)){
+        printk(KERN_ERR "class_create failed.");
+        return PTR_ERR(cls);
+    }
     return 0;
 }
 
 static void __exit cleanup_mod(void){//後始末
     cdev_del(&cdv);
+    class_destroy(cls);
     unregister_chrdev_region(dev, 1);
     printk(KERN_INFO "%s is unloaded. major:%d\n",__FILE__,MAJOR(dev));
 }
